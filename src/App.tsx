590e2aa97615d414aa7438c2e2bc618f49d93e99
Norphel <113838216+norphel@users.noreply.github.com>
2024-02-12 15:29:32 +0530

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 import Header from "./components/Header";
-import Products from "./components/Home";
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
   return (
     <>
       <Header numberOfItemsInCart={cartItems.length} />
-      <Products />
+      <Outlet />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App.tsx";
+import Products from "./components/Home.tsx";
+import Cart from "./components/Cart.tsx";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Products />,
+      },
+      {
+        path: "cart",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
