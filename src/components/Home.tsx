347a6871deabbb30b9933e022157bc85230dcdf7
Norphel <113838216+norphel@users.noreply.github.com>
2024-02-16 12:39:12 +0530

    style home and products page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,17 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <Link to={"products"}>
-      <button>Shop Now</button>
-    </Link>
+    <main className="flex-grow flex flex-col justify-center items-center gap-7">
+      <h2 className="text-center text-2xl md:text-4xl lg:text-6xl">
+        Welcome <span className="block">to</span>{" "}
+        <span className="block font-semibold">Dummy Store</span>
+      </h2>
+      <Link to={"products"}>
+        <button className="rounded-xl px-6 py-2 bg-green-600 text-white font-semibold text-md md:text-xl lg:text-2xl">
+          Shop Now
+        </button>
+      </Link>
+    </main>
   );
 };
 export default Home;
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -9,19 +9,45 @@ interface CardProp {
 const ProductCard = ({ product }: CardProp) => {
   return (
     <Link to={`/products/${product.id}`} state={{ product }}>
-      <div className="flex flex-col border p-2 gap-2 hover:cursor-pointer">
+      <div className="flex flex-col border p-2 gap-2 hover:cursor-pointer min-h-80">
         <img
           src={product.thumbnail}
           alt={`${product.title} thumbnail`}
           className="min-h-40 max-h-40 object-cover object-center"
         />
-        <div className="flex flex-col justify-between h-full gap-4">
+        <div className="flex flex-col justify-between h-full gap-4 flex-grow">
           <p className="text-xl md:text-2xl lg:text-3xl text-slate-800">
             {product.title}
           </p>
           <div className="flex justify-between items-center">
             <p className="text-lg md:text-xl lg:text-2xl">{`$${product.price}`}</p>
-            <p>{product.rating}</p>
+            <div className="flex gap-2">
+              <svg
+                width="20px"
+                height="20px"
+                viewBox="0 0 64 64"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+                aria-hidden="true"
+                role="img"
+                preserveAspectRatio="xMidYMid meet"
+                fill="#000000"
+              >
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g
+                  id="SVGRepo_tracerCarrier"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path
+                    d="M62 25.2H39.1L32 3l-7.1 22.2H2l18.5 13.7l-7 22.1L32 47.3L50.5 61l-7.1-22.2L62 25.2z"
+                    fill="#ffce31"
+                  ></path>
+                </g>
+              </svg>
+              <p>{product.rating}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -56,7 +82,58 @@ const Products = () => {
   }, []);
 
   if (loading) return <LoadingSpinner />;
-  if (error) return <p>A network error was encountered</p>;
+  if (error)
+    return (
+      <div className="flex-grow flex flex-col justify-center items-center">
+        <svg
+          width="100px"
+          height="100px"
+          viewBox="0 0 400 400"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M158.883 171.858C157.302 163.63 157.661 155.331 157.661 147.001"
+              stroke="#000000"
+              strokeOpacity="0.9"
+              strokeWidth="16"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>{" "}
+            <path
+              d="M243.182 168.082C244.769 160.655 243.182 152.692 243.182 145"
+              stroke="#000000"
+              strokeOpacity="0.9"
+              strokeWidth="16"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>{" "}
+            <path
+              d="M149 263C175.051 221.917 226.044 220.551 255 257.987"
+              stroke="#000000"
+              strokeOpacity="0.9"
+              strokeWidth="16"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>{" "}
+          </g>
+        </svg>
+        <p>A network error was encountered</p>
+        <Link to="/">
+          <button className="border rounded-lg px-6 py-3 bg-slate-300">
+            Try Again!
+          </button>
+        </Link>
+      </div>
+    );
 
   return (
     <main>
