7e5f0367fa42dfcb27b7a9ab20bd3842f11457de
Norphel <113838216+norphel@users.noreply.github.com>
2024-02-20 16:47:29 +0530

    style image carousel

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -22,30 +22,81 @@ const ImageCarousel = ({ images }: ImageCarouselProps) => {
   useEffect(() => {
     const interval = setInterval(() => {
       nextImage();
-    }, 2000);
+    }, 5000);
 
     return () => clearInterval(interval);
   }, [activeIndex]);
 
   return (
-    <div className="relative">
+    <div className="relative max-h-80 max-w-80 sm:max-w-md md:max-w-lg lg: lg:max-w-xl">
       <img
         src={images[activeIndex]}
         alt={`Slide ${activeIndex + 1}`}
-        className="w-full"
+        className="w-full max-h-80 min-h-80 object-contain border"
       />
       <div className="flex">
         <button
           onClick={prevImage}
-          className="absolute left-0 top-1/2 transform -translate-y-1/2"
+          className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-slate-300"
         >
-          Prev
+          <svg
+            fill="#000000"
+            version="1.1"
+            baseProfile="tiny"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            width="34px"
+            height="64px"
+            viewBox="0 0 42 42"
+            xmlSpace="preserve"
+            transform="matrix(-1, 0, 0, 1, 0, 0)"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <polygon
+                fillRule="evenodd"
+                points="11,38.32 28.609,21 11,3.68 13.72,1 34,21.01 13.72,41 "
+              ></polygon>{" "}
+            </g>
+          </svg>
         </button>
         <button
           onClick={nextImage}
-          className="absolute right-0 top-1/2 transform -translate-y-1/2"
+          className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-slate-300"
         >
-          Next
+          <svg
+            fill="#000000"
+            version="1.1"
+            baseProfile="tiny"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            width="34px"
+            height="64px"
+            viewBox="0 0 42 42"
+            xmlSpace="preserve"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              {" "}
+              <polygon
+                fillRule="evenodd"
+                points="11,38.32 28.609,21 11,3.68 13.72,1 34,21.01 13.72,41 "
+              ></polygon>{" "}
+            </g>
+          </svg>
         </button>
       </div>
 
